2ea53913afbf67611b81aea5b9e48ae866cbcfe2
hwandam77 <junghwandam@gmail.com>
2026-04-15 08:11:23 +0900

    refactor(ddd): refine UrlFavorites::UseCases::Result

--- app/url_favorites/use_cases/result.rb
+++ app/url_favorites/use_cases/result.rb
@@ -2,7 +2,16 @@
 
 module UrlFavorites
   module UseCases
-    Result = Struct.new(:ok?, :value, :error, keyword_init: true)
+    Result = Struct.new(:ok, :value, :error, keyword_init: true) do
+      def ok? = ok
+
+      def self.ok(value:)
+        new(ok: true, value: value, error: nil)
+      end
+
+      def self.fail(error:)
+        new(ok: false, value: nil, error: error)
+      end
+    end
   end
 end
-
